20fb178888bb24ab91ff1d2f86eb3e4a5f63cc72
Mark Anthony Cabanlit <anthony.cabanlit@lanexus.com>
2022-04-29 15:15:53 +0800

    Default AutoField
    Updated settings.py to address DEFAULT_AUTO_FIELD

--- martor_demo/martor_demo/settings.py
+++ martor_demo/martor_demo/settings.py
@@ -145,3 +145,5 @@ MEDIA_URL = '/media/'
 # MEDIA_ROOT = os.path.join(tempfile.gettempdir(), 'martor_media')
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
